c0e0ff00a97f8839deebe7d099481af54b3d8410
88saiba <kuroganesaiba@gmail.com>
2024-09-16 06:22:11 +0700

    implement lilbit functional

--- gitpush.exs
+++ gitpush.exs
@@ -1,6 +1,6 @@
 defmodule GitPush do
   @username "<YOUR_USERNAME>"
-  @token "<YOUR_PERSONAL_ACCESS_TOKEN>"
+  @token "<YOUR_PERSONAL_ACCESS_TOKENs>"
   @credentials_file System.user_home!() <> "/.git-credentials"
 
   def run([commit_message]) do
